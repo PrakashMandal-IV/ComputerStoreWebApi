f7a10d35cf76ce92e82b0d2aba8b06b6e59310bf
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-04-18 22:00:55 +0530

    Add : Address Model

---
+++ Data/Model/Address.cs
@@ -0,0 +1,17 @@
+﻿namespace ComputerStoreBackEnd.Data.Model
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AddressType { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string LandMark { get; set; }
+        public int PostalCode { get; set; }
+        public int PhoneNumber { get; set; }
+
+    }
+}
