47a0e2f37d03bb91c83bbcb4ed9f9c0e6fd2dc4a
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-04-18 21:58:54 +0530

    Add : Order Model

---
+++ Data/Model/Orders.cs
@@ -0,0 +1,20 @@
+﻿namespace ComputerStoreBackEnd.Data.Model
+{
+    public class Orders
+    {
+        public int Id { get; set; } 
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public int ModeOfPayment { get; set; }
+        public int IsCompleted { get; set; }
+        public int? IsReturned { get; set; }
+        public int? IsRefunded { get; set; }
+        public DateTime CreatedBy { get; set; }
+        public int? CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime DeletedAt { get; set; }
+        public int DeletedBy { get; set; }
+
+    }
+}
