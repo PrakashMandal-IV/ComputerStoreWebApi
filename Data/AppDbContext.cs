3af22da145405660f565cda98ca077e0468efd5b
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-04-19 13:24:11 +0530

    Add : DB context file and connection string

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ComputerStoreBackEnd.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputerStoreBackEnd.Data
+{
+    public class AppDbContext:DbContext
+    {
+       public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Admin> Admin { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var MyAllowSpecificOrigin = "_myAllowSpecificOrigin";
 builder.Services.AddCors(options =>
 {
