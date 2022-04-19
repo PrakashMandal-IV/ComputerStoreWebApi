a29041cd8b7f556ded96c8dc6858971f4e250bf9
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-04-20 00:36:28 +0530

    Change : If statment on App Db Initializer

--- Data/AppDbInitializer.cs
+++ Data/AppDbInitializer.cs
@@ -10,7 +10,8 @@ namespace ComputerStoreWebApi.Data
             using(var serviceScope= applicationbuilder.ApplicationServices.CreateScope())
             {
                 var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
-                
+                if(!context.Admin.Any())
+                { 
                     context.Admin.Add(new Admin()
                     {
                         UserName = "Admin",
