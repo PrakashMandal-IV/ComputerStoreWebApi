efc0724e97f038cf4cd15dcda984650d8aabcbfe
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-04-19 19:28:03 +0530

    Change : Db Initilizer

---
+++ Data/AppDbInitializer.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+using ComputerStoreBackEnd.Controllers;
+using ComputerStoreBackEnd.Data;
+using ComputerStoreWebApi.Data;
+
+namespace ComputerStoreWebApi.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationbuilder)
+        {
+            using(var serviceScope= applicationbuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+                if(!context.Admin.Any())
+                {
+                    context.Admin.Add(new Admin()
+                    {
+
+                    });
+                        
+
+                    
+                }
+            }
+        }
+    }
+}
