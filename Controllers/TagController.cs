f94379faa7e981222c9ee0eb8710e93d79905067
Prakash <prakashmandal.iv@gmail.com>
2022-04-29 13:34:20 +0530

    Add : Tag controller

---
+++ Controllers/TagController.cs
@@ -0,0 +1,30 @@
+﻿using ComputerStoreWebApi.Data.Services;
+using ComputerStoreWebApi.Data.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerStoreWebApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class TagController : Controller
+    {
+        public TagService _tagService;
+        public TagController(TagService tagService)
+        {
+            _tagService = tagService;
+        }
+
+        [HttpPost("add-tag")]
+        public IActionResult AddTag([FromBody]TagVM tag)
+        {
+            _tagService.AddTag(tag);
+            return Ok();
+        }
+        [HttpGet("get-tag-by-name")]
+        public IActionResult GetTagByName(string name)
+        {
+            var tag = _tagService.GetTagByName(name);
+            return Ok(tag);
+        }
+    }
+}
