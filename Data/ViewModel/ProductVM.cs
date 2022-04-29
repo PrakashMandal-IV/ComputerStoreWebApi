e336cdb3d17428936d1f4771019b16f627bd0ccc
Prakash <prakashmandal.iv@gmail.com>
2022-04-29 19:56:51 +0530

    Add : Product VM

---
+++ Data/ViewModel/ProductVM.cs
@@ -0,0 +1,14 @@
+﻿namespace ComputerStoreWebApi.Data.ViewModel
+{
+    public class ProductVM
+    {       
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public decimal NewPrice { get; set; }
+        public int InStock { get; set; }
+        public int CreatedBy { get; set; }
+        public List<int> CategoryId { get; set; }
+    }
+}
