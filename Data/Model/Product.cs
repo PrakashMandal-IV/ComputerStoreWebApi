7495a5ef3e042ab384ff6d56e34ec197ebf062a6
Prakash <prakashmandal.iv@gmail.com>
2022-05-03 12:48:47 +0530

    Fix : product price nullable

--- Data/Model/Product.cs
+++ Data/Model/Product.cs
@@ -9,8 +9,8 @@ namespace ComputerStoreWebApi.Data.Model
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
-        public int? Price { get; set; }
-        public int? NewPrice { get; set; }
+        public int Price { get; set; }
+        public int NewPrice { get; set; }
         public int InStock { get; set; }            
         public int? CreatorId { get; set; }
         public Admin Creator { get; set; } = default!;
