e28fb1a8263ab698a582b0bb4322804f4e9e18ea
Prakash <prakashmandal.iv@gmail.com>
2022-05-04 09:24:43 +0530

    Add : Login in http request that return account id in plain text

---
+++ Controllers/loginController.cs
@@ -0,0 +1,35 @@
+﻿using ComputerStoreWebApi.Data.Services;
+using ComputerStoreWebApi.Data.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerStoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class loginController : ControllerBase
+    {
+        private readonly LoginService? _loginService;
+        public loginController(LoginService loginService)
+        {
+            _loginService = loginService;
+        }
+        [HttpPut("admin-login")]
+        public IActionResult AdminLogin([FromBody]AdminLoginVM adminLogin)
+        {
+            var _response = _loginService.AdminLogin(adminLogin);
+            if(_response == null)
+            {
+                return NotFound();
+            }
+            else if(_response== "Invalid Detail")
+            {
+                return BadRequest("Invalid Details");
+            }
+            else
+            {
+                return Ok(_response);
+            }
+        }
+    }
+}
