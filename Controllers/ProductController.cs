4fbbae303be205f38ad9c17441fdb23a737a7178
Prakash <prakashmandal.iv@gmail.com>
2022-05-06 22:16:59 +0530

    Fix : Await task method

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -27,64 +27,45 @@ namespace ComputerStoreWebApi.Controllers
         public async Task<IActionResult> GetProduct()
         {         
                 var _product = await Task.Run(()=> _productService.GetAllProduct());
-                return Ok(_product);
-            
-            
+                return Ok(_product);         
         }
         [HttpPut("update-product-name/{id}")]
         public async Task<IActionResult> UpdateProductName(int id,[FromBody] ProductVMbyName product)
-        {
-            await Task.Run(() =>
-            {
+        {          
                 string email = User.FindFirstValue(ClaimTypes.Email);
-                var _response = _productService.UpdateProductName(id, product, email);
-                return Ok(_response);
-            });
-            return Ok();
+                var _response = await Task.Run(() => _productService.UpdateProductName(id, product, email));
+                return Ok(_response);  
         }
         [HttpPut("update-product-description/{id}")]
         public async Task<IActionResult> UpdateProductDescription(int id,[FromBody] ProductVMbyDescription product)
         {
-            await Task.Run(() =>
-            {
                 string email = User.FindFirstValue(ClaimTypes.Email);
-                var _response = _productService.UpdateProductDescription(id, product, email);
+                var _response = await Task.Run(() => _productService.UpdateProductDescription(id, product, email));
                 return Ok(_response);
-            });
-            return Ok();           
+                     
         }
         [HttpPut("update-product-image/{id}")]
         public async Task<IActionResult> UpdateProductImage(int id, [FromBody] ProductVMImage product)
-        {
-            await Task.Run(() =>
-            {
+        {          
                 string email = User.FindFirstValue(ClaimTypes.Email);
-                var _response = _productService.UpdateProductImage(id, product, email);
+                var _response = await Task.Run(() => _productService.UpdateProductImage(id, product, email));
                 return Ok(_response);
-            });
-            return Ok();
         }
         [HttpPut("update-product-price/{id}")]
         public async Task<IActionResult> UpdateProductprice(int id, [FromBody] ProductVMPrice product)
-        {
-            await Task.Run(() =>
-            {
+        {      
                 string email = User.FindFirstValue(ClaimTypes.Email);
-                var _response = _productService.UpdateProductPrice(id, product, email);
+                var _response = await Task.Run(() => _productService.UpdateProductPrice(id, product, email));
                 return Ok(_response);
-            });
-            return Ok();
+           
         }
         [HttpPut("update-product-stock/{id}")]
         public async Task<IActionResult> UpdateProductStock(int id, [FromBody] ProductVMStock product)
         {
-            await Task.Run(() =>
-            {
+           
                 string email = User.FindFirstValue(ClaimTypes.Email);
-                var _response = _productService.UpdateProductStock(id, product, email);
-                return Ok(_response);
-            });
-            return Ok();
+                var _response = await Task.Run(() => _productService.UpdateProductStock(id, product, email));
+                return Ok(_response);           
         }
     }
 }
