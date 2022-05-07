b91a5abb873c8576808e4de78b4e25a7d04b6638
Prakash <prakashmandal.iv@gmail.com>
2022-05-07 19:28:31 +0530

    Change : Order table

--- Data/Model/Orders.cs
+++ Data/Model/Orders.cs
@@ -3,19 +3,19 @@
     public class Orders
     {
         public int Id { get; set; } 
-        public int ProductId { get; set; }
+        public int? ProductId { get; set; }
         public Product? Product { get; set; }     
-        public int ModeOfPayment { get; set; }
-        public int AddressId { get; set; }
+        public bool Paid { get; set; }
+        public int? AddressId { get; set; }
         public Address? Address { get; set; } //refrence
         public string Status { get; set; } = "pending";
         public int CreatorId { get; set; } 
         public User? Creator { get; set; }
         public DateTime CreatedAt { get; set; }
-        public int ModifiedId { get; set; }
-        public DateTime ModifiedBy { get; set; }
-        public DateTime DeletedAt { get; set; }
-        public int DeletedBy { get; set; }
+        public int? ModifiedId { get; set; }
+        public DateTime? ModifiedBy { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public int? DeletedBy { get; set; }
 
         
        public List<UserOrder> UserOrders { get; set; } = default!;
