c41f9863b9605e682e5d52f93fbfd5ab81fcfb54
Prakash <prakashmandal.iv@gmail.com>
2022-05-07 19:32:53 +0530

    Add : Order controller

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ComputerStoreWebApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    [Authorize]
+    public class OrdersController : Controller
+    {
+        private readonly OrdersService _ordersService;
+        public OrdersController(OrdersService ordersService)
+        {
+          _ordersService = ordersService;
+        }
+        [HttpPost("generate-order")]
+        public async Task<IActionResult> GenerateOrder([FromBody]OrderVM order)
+        {
+            string email = User.FindFirstValue(ClaimTypes.Email);
+            await Task.Run(() => _ordersService.CreateOrder(order, email));
+            return Ok();
+        }
+    }
+}
