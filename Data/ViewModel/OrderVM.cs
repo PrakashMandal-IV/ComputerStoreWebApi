efd7709ce6c88b922d9d9599cf85df72b9c9c543
Prakash <prakashmandal.iv@gmail.com>
2022-05-07 19:29:38 +0530

    Add : VM for orders

--- Data/ViewModel/OrderVM.cs
+++ Data/ViewModel/OrderVM.cs
@@ -1,10 +1,8 @@
 ﻿namespace ComputerStoreWebApi.Data.ViewModel
 {
     public class OrderVM
-    {
+    {   
         public int ProductId { get; set; }
-        public int UserId { get; set; }
-        public int ModeOfPayment { get; set; }
-        public int AddressId { get; set; }
+        public bool Paid { get; set; }      
     }
 }
