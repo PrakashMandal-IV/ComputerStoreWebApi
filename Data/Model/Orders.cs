23bc6e4c722d6f785c94bd4222ee9a9f384bbb58
Prakash <prakashmandal.iv@gmail.com>
2022-05-07 10:56:35 +0530

    Changes : Order model

--- Data/Model/Orders.cs
+++ Data/Model/Orders.cs
@@ -4,15 +4,17 @@
     {
         public int Id { get; set; } 
         public int ProductId { get; set; }
+        public Product? Product { get; set; }
         public int UserId { get; set; }
         public int ModeOfPayment { get; set; }
         public int AddressId { get; set; }
         public Address? Address { get; set; } //refrence
         public string Status { get; set; } = "pending";
-        public DateTime CreatedBy { get; set; }
-        public int? CreatedAt { get; set; }
-        public DateTime ModifiedAt { get; set; }
-        public int ModifiedBy { get; set; }
+        public int CreatorId { get; set; } 
+        public User? Creator { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int ModifiedId { get; set; }
+        public DateTime ModifiedBy { get; set; }
         public DateTime DeletedAt { get; set; }
         public int DeletedBy { get; set; }
 
