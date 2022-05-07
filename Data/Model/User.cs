ad17be9f910daaccc35566ef85dba0ddd92f53d9
Prakash <prakashmandal.iv@gmail.com>
2022-05-07 01:33:19 -0500

    Add: user order and relation

--- Data/Model/User.cs
+++ Data/Model/User.cs
@@ -8,11 +8,14 @@
         public string Password { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public int PhoneNumber { get; set; }
-        public DateTime CreatedBy { get; set; }
-        public int ? CreatedAt { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
         public int ModifiedBy { get; set; }
         public DateTime DeletedAt { get; set; }
         public int DeletedBy { get; set; }
+
+       
+      public List<UserOrder> UserOrder { get; set; } = default!;
     }
 }
---
+++ Data/Model/UserOrder.cs
@@ -0,0 +1,14 @@
+﻿namespace ComputerStoreWebApi.Data.Model
+{
+    public class UserOrder
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; } = default!;
+
+        public int OrderId { get; set; }
+        public Orders Order { get; set; } = default!;
+       
+    }
+}
