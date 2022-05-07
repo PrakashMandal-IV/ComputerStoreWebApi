21261c20e6d9e81f567f5f339308887d9abc189a
Prakash <prakashmandal.iv@gmail.com>
2022-05-07 10:51:36 +0530

    Change : Status on orders

--- Data/Model/Orders.cs
+++ Data/Model/Orders.cs
@@ -6,9 +6,9 @@
         public int ProductId { get; set; }
         public int UserId { get; set; }
         public int ModeOfPayment { get; set; }
-        public int IsCompleted { get; set; }
-        public int? IsReturned { get; set; }
-        public int? IsRefunded { get; set; }
+        public int AddressId { get; set; }
+        public Address? Address { get; set; } //refrence
+        public string Status { get; set; } = "pending";
         public DateTime CreatedBy { get; set; }
         public int? CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
