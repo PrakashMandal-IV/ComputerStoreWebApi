bf4ef2d87ac332785f495a6930b0dbf0a57bc11b
Prakash <prakashmandal.iv@gmail.com>
2022-05-22 21:18:40 +0530

    Add : user address model

---
+++ Data/Model/UserAddress.cs
@@ -0,0 +1,13 @@
+﻿namespace ComputerStoreWebApi.Data.Model
+{
+    public class UserAddress
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; } = default!;
+
+        public int AddressId { get; set; }
+        public Address Address { get; set; } = default!;
+    }
+}
